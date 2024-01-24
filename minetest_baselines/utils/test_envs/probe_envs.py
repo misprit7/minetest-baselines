007c14346db5d35cc66a75a259942c3e8542c77b
Aileen <aileenz240120@gmail.com>
2024-01-24 12:39:09 -0800

    Updated probe environments

--- minetest_baselines/utils/test_envs/probe_envs.py
+++ minetest_baselines/utils/test_envs/probe_envs.py
@@ -225,3 +225,49 @@ class TwoActionTwoObservationTwoReward(gym.Env):
 
     def close(self):
         return
+
+class TwoActionTwoRewardHundredSteps(gym.Env):
+    metadata = {'render_modes': ['human', 'rgb_array']}
+
+    def __init__(self, **kwargs):
+        self.observation_shape = (128, 128, 1)
+        self.observation_space = spaces.Box(low = np.zeros(self.observation_shape), 
+                                            high = np.ones(self.observation_shape) * 255,)
+        self.action_space = spaces.Discrete(2)
+        self.terminated = False
+        self.stepnum = 0
+
+    def step(self, action):
+        observation = self._get_obs()
+        info = self._get_info()
+
+        reward = 1 if action == 1 else -1
+        self.stepnum += 1
+
+        if self.stepnum >= 100:
+            self.terminated = True
+
+        return observation, reward, self.terminated, False, info
+
+    def _get_obs(self):
+        return np.ones(self.observation_shape, dtype = np.float32) * 255
+
+    def _get_info(self):
+        if self.terminated:
+            return {"terminal_observation": self._get_obs}
+        else:
+            return {}
+
+    def reset(self, seed=None, options=None):
+        # We need the following line to seed self.np_random
+        super().reset(seed=seed)
+
+        observation = self._get_obs()
+        info = self._get_info()
+        self.terminated = False
+        self.stepnum = 0
+
+        return observation, info
+
+    def close(self):
+        return
