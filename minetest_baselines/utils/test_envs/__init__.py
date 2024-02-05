c2fe11cfb9950db5382095e09dbb682957dfab12
Aileen <aileenz240120@gmail.com>
2024-02-04 23:33:09 -0800

    Updated probe envs and debugging

--- minetest_baselines/utils/test_envs/__init__.py
+++ minetest_baselines/utils/test_envs/__init__.py
@@ -1,22 +1,19 @@
-
 import gymnasium as gym
 from gymnasium.envs.registration import register
 from minetest_baselines.utils.test_envs.probe_envs import OneActionOneReward
 from minetest_baselines.utils.test_envs.probe_envs import OneActionOneRewardTwoStep
 from minetest_baselines.utils.test_envs.probe_envs import TwoActionTwoReward
 from minetest_baselines.utils.test_envs.probe_envs import TwoActionTwoObservationTwoReward
-from minetest_baselines.utils.test_envs.probe_envs import TwoActionTwoRewardHundredSteps
+from minetest_baselines.utils.test_envs.probe_envs import TwoActionTwoRewardThirtyTwoSteps
+from minetest_baselines.utils.test_envs.probe_envs import OneActionOneRewardThirtyTwoSteps
+from minetest_baselines.utils.test_envs.probe_envs import TwoActionTwoObservationTwoRewardThirtyTwoSteps
 
+
+## For DQN, PPO, etc.
 register(
     id='TwoActionTwoReward-v0',
     entry_point='minetest_baselines.utils.test_envs:TwoActionTwoReward',
     max_episode_steps=1,
-)
-
-register(
-    id='TwoActionTwoRewardHundredSteps-v0',
-    entry_point='minetest_baselines.utils.test_envs:TwoActionTwoRewardHundredSteps',
-    max_episode_steps=100,
 )
 
 register(
@@ -35,4 +32,24 @@ register(
     id='OneActionOneRewardTwoStep-v0',
     entry_point='minetest_baselines.utils.test_envs:OneActionOneRewardTwoStep',
     max_episode_steps=2,
+)
+
+## For Muax, EfficientZero, etc
+register(
+    id='OneActionOneRewardThirtyTwoSteps-v0',
+    entry_point='minetest_baselines.utils.test_envs:OneActionOneRewardThirtyTwoSteps',
+    max_episode_steps=32,
+)
+
+register(
+    id='TwoActionTwoRewardThirtyTwoSteps-v0',
+    entry_point='minetest_baselines.utils.test_envs:TwoActionTwoRewardThirtyTwoSteps',
+    max_episode_steps=32,
+)
+
+
+register(
+    id='TwoActionTwoObservationTwoRewardThirtyTwoSteps-v0',
+    entry_point='minetest_baselines.utils.test_envs:TwoActionTwoObservationTwoRewardThirtyTwoSteps',
+    max_episode_steps=32,
 )
