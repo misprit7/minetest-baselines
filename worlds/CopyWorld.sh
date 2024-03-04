513c2111c716cae2e5eb6373d15c1eea2435df6a
Anthony <gimmegold500@gmail.com>
2024-03-03 21:37:12 -0800

    updated Copyworld and created CleanWorlds

--- worlds/CopyWorld.sh
+++ worlds/CopyWorld.sh
@@ -2,10 +2,24 @@
 
 echo "hello world"
 
-rm -r ToCopy/WorldTrain
-mkdir ToCopy/WorldTrain
+rm -r ToCopy
+mkdir ToCopy
 
-cp -r ToCopy/World1/. ToCopy/WorldTrain
+if [[ -z $@ ]]; then
+   num=5
+else
+   num=$@
+fi
+
+echo $num
+
+for ((i = 0 ; i < $num ; i++ ))
+do
+   #echo "Welcome $i times"
+   cp -r World_Train ToCopy 
+   mv ToCopy/World_Train ToCopy/$i
+
+done
 
 echo "Complete!"
 
